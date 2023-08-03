7196e230e5b01abc7d8526de9ff64c949c9d16e1
Ricardo Gauto <ricardo.gauto@redtecnologica.org>
2023-08-03 13:00:34 -0300

    Asignacion y aritmetica en PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Asignacion
+$num = 9;
+$lang = [
+    'es' => 'Espanol',
+    'en' => 'Ingles'
+];
+
+// Aritmetica
+echo "Suma 2 + 2" . ((int) 2 + (int) 2);
+echo "Resta 20 - 9" . ((int) 20 - (int) 9);
+echo "Multiplicacion 3 * 3" . 3 * 3;
+echo "Divide 20 / 2" . 20 / 2;
+echo "Modulo 23 % 4" . 23 % 4;
+echo "Exponencial 2 ** 3" . 2 ** 3;
